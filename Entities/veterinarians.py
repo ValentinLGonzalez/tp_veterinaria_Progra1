c3aafe1e44c31fc95efb4e4da4a1cf497b765199
Lucas Barbosa <barbosalucasroberto@gmail.com>
2025-09-10 00:14:32 -0300

    Fixed modify_veterinarian_action wiht the new function for update

--- Entities/veterinarians.py
+++ Entities/veterinarians.py
@@ -1,7 +1,7 @@
 from utils.arrayHelper import print_array_bidimensional
 from utils.arrayHelper import print_array
 from utils.constants import HEADER_VETERINARIAN
-from utils.entitiesHelper import get_next_id
+from utils.entitiesHelper import get_next_id, update_entity_data
 
 def create_veterinarian(array_veterinarians):
     """Creates a new veterinarian and appends it to the existing list.
@@ -59,14 +59,27 @@ def read_veterinarian_by_id(veterinarian_id, array_veterinarians):
     for veterinarian in array_veterinarians:
         if(veterinarian[HEADER_VETERINARIAN.index("veterinarian_id")] == veterinarian_id and veterinarian[HEADER_VETERINARIAN.index("active")] == True):
             return veterinarian
+    return None
                   
-def update_veterinarian_by_id(veterinarian_id, updated_veterinarian, array_veterinarians):
+def update_veterinarian_by_id(updated_veterinarian, array_veterinarians):
+    """Updates a veterinarian in the list by its ID.
+
+    Searches for the veterinarian with the same ID as `updated_veterinarian`
+    and replaces the old record with the new one. If the ID does not exist,
+    the function returns None.
+
+    Args:
+        updated_veterinarian (list): The veterinarian record with updated values.
+        array_veterinarians (list[list]): The list of veterinarians, where each
+            veterinarian is represented as a list of values.
+    """
     current_veterinarians_id = [veterinarian[HEADER_VETERINARIAN.index("veterinarian_id")] for veterinarian in array_veterinarians]
-    if(current_veterinarians_id.count(veterinarian_id)):
-        updated_veterinarian_index = current_veterinarians_id.index(veterinarian_id)
-        for i in range(len(array_veterinarians[updated_veterinarian_index])):
-            array_veterinarians[updated_veterinarian_index][i] = updated_veterinarian[i]
-    return array_veterinarians[updated_veterinarian_index]
+    updated_veterinarian_id = updated_veterinarian[HEADER_VETERINARIAN.index("veterinarian_id")]
+    if(updated_veterinarian_id in current_veterinarians_id):
+        updated_veterinarian_index = current_veterinarians_id.index(updated_veterinarian_id)
+        array_veterinarians[updated_veterinarian_index] = updated_veterinarian
+        return array_veterinarians[updated_veterinarian_index]
+    return None
 
         
 def delete_veterinarian_by_id(veterinarian_id, array_veterinarians):
@@ -76,8 +89,6 @@ def delete_veterinarian_by_id(veterinarian_id, array_veterinarians):
         array_veterinarians[deleted_veterinarian_index][HEADER_VETERINARIAN.index("active")] = False
     
 def show_veterinarian(veterinarian): 
-    print()
-    print("Veterinario agregado correctamente.")
     print()
     print_array(HEADER_VETERINARIAN, veterinarian)
 
@@ -94,12 +105,30 @@ def add_veterinarian_action(veterinarians):
     show_veterinarian(new_veterinarian)
      
 def modify_veterinarian_action(veterinarians):
-    dni_input = input("Ingrese el DNI del Veterinario que desea modificar: ")
-    veterinarian_to_update = get_veterinarian_by_dni(dni_input, veterinarians)
-    print(veterinarian_to_update)
-    show_veterinarian(veterinarian_to_update)
-    updated_veterinarian = create_veterinarian(veterinarians)
-    return update_veterinarian_by_id(updated_veterinarian[HEADER_VETERINARIAN.index("veterinarian_id")], updated_veterinarian, veterinarians)
+    """Modifies an existing veterinarian by searching with DNI.
+
+    Continuously prompts the user to enter a veterinarian's DNI until a valid
+    veterinarian is found. Once found, the veterinarian's details are displayed,
+    updated with user input, and saved back into the list.
+
+    Args:
+        veterinarians (list[list]): The list of veterinarians, where each
+            veterinarian is represented as a list of values.
+
+    Returns:
+        list: The updated veterinarian record.
+    """
+    is_valid_dni = False
+    while not is_valid_dni:
+        dni_input = input("Ingrese el DNI del Veterinario que desea modificar: ")
+        veterinarian_to_update = get_veterinarian_by_dni(dni_input, veterinarians)
+        if veterinarian_to_update:
+            is_valid_dni = True
+            show_veterinarian(veterinarian_to_update)
+        else:
+            print("El DNI no corresponde a un veterinario existente.")
+    updated_veterinarian = update_entity_data(veterinarian_to_update ,HEADER_VETERINARIAN)
+    return update_veterinarian_by_id(updated_veterinarian, veterinarians)
 
 def show_all_veterinarians_action(array_veterinarians): 
     active_veterinarians = list(filter(lambda v: v[HEADER_VETERINARIAN.index("active")] == True, array_veterinarians))
