22311062c9cd9e5436680cc0e336b33b57ca2bb2
ValentinLGonzalez <valentinlgonzalez@uade.edu.ar>
2025-10-27 19:26:10 -0300

    se corrige una linea particular que no haciasentido

--- entities/owners/validations.py
+++ entities/owners/validations.py
@@ -2,11 +2,6 @@ import re
 
 def is_valid_name(input):
     """
-        Validates if a text string has the input format only alphanimeric chars
-    Args:
-        input: The text string to validate
-
-    Returns:
-        True if the string meets the format, False otherwise
+    si el string matchea el formato, true. sino false
     """
     return bool(re.fullmatch(r'^[A-Za-z]+$', input))
