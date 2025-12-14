9f168ebf774479059df2e041e1685ffd5ad306af
Lucas Barbosa <barbosalucasroberto@gmail.com>
2025-12-14 16:06:55 -0300

    Added modify y new veterinarian ui

--- entities/veterinarians/view.py
+++ entities/veterinarians/view.py
@@ -1,17 +1,139 @@
+import re
 import tkinter as tk
 from tkinter import messagebox
 from entities.veterinarians.controller import show_all_veterinarians_action
-from entities.veterinarians.entity import READABLE_HEADER
+from entities.veterinarians.data import get_next_veterinarian_id, save_data_veterinarian, update_data_veterinarian
+from entities.veterinarians.entity import READABLE_HEADER, delete_veterinarian_by_id, get_readable_veterinarian, get_veterinarian_by_id
 from utils.constants import HEADER_VETERINARIAN
 
-def modify_veterinarian(id):
-    print(f"Editando ID: {id}")
+def on_create_new_veterinarian(root, container):
+    modal_modify = tk.Toplevel(root)
+    modal_modify.title(f"Crear nuevo veterinario")
+    modal_modify.geometry("500x400")
+    
+    new_veterinarian = []
+    hasError = False
+    for prop in HEADER_VETERINARIAN:
+        if prop == "veterinarian_id":
+            new_veterinarian.append(get_next_veterinarian_id())
+        elif prop == "dni":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_dni = tk.Entry(modal_modify)
+            input_dni.pack(fill="x", padx=10)
+        elif prop == "nombre":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_name = tk.Entry(modal_modify)
+            input_name.pack(fill="x", padx=10)
+        elif prop == "apellido":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_surname = tk.Entry(modal_modify)
+            input_surname.pack(fill="x", padx=10)
+        elif prop == "matricula":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_matricula = tk.Entry(modal_modify)
+            input_matricula.pack(fill="x", padx=10)
+        elif prop == "email":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_email = tk.Entry(modal_modify)
+            input_email.pack(fill="x", padx=10)
+        elif prop == "telefono":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_telefono = tk.Entry(modal_modify)
+            input_telefono.pack(fill="x", padx=10)
+    def save_data():
+        if not hasError:
+            new_veterinarian.append(input_dni.get())
+            new_veterinarian.append(input_name.get())
+            new_veterinarian.append(input_surname.get())
+            new_veterinarian.append(input_matricula.get())
+            new_veterinarian.append(input_email.get())
+            new_veterinarian.append(input_telefono.get())
+            new_veterinarian.append(True)
+            save_data_veterinarian(new_veterinarian)
+            modal_modify.destroy()
+            load_dynamic_table(container, root)
 
-def delete_veterinarian(id):
+    btn_frame = tk.Frame(modal_modify, pady=20)
+    btn_frame.pack()
+    
+    tk.Button(btn_frame, text="Cancelar", command=modal_modify.destroy).pack(side="left", padx=5)
+    tk.Button(btn_frame, text="Guardar", bg="#4CAF50", fg="white", command=save_data).pack(side="left", padx=5)
+
+def on_search_veterinarian(input_search, list_frame):
+    texto_busqueda = input_search.get().lower()
+    
+    entities = show_all_veterinarians_action()
+    string_array = [('|').join(list(e)) for e in entities]
+    for entity in string_array:
+        match = texto_busqueda.lower() in entity.lower()
+        if match:
+            entity_tuple = get_readable_veterinarian(entity.split('|'))
+            load_dynamic_table(list_frame, entity_tuple)
+            
+    
+    
+def modify_veterinarian(id, root, container):
+    modal_modify = tk.Toplevel(root)
+    modal_modify.title(f"Editar Usuario ID: {id}")
+    modal_modify.geometry("500x400")
+
+    current_data = get_veterinarian_by_id(id)
+
+    for prop in HEADER_VETERINARIAN:
+        if prop == "dni":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_dni = tk.Entry(modal_modify)
+            input_dni.pack(fill="x", padx=10)
+            input_dni.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+        elif prop == "nombre":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_name = tk.Entry(modal_modify)
+            input_name.pack(fill="x", padx=10)
+            input_name.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+        elif prop == "apellido":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_surname = tk.Entry(modal_modify)
+            input_surname.pack(fill="x", padx=10)
+            input_surname.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+        elif prop == "matricula":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_matricula = tk.Entry(modal_modify)
+            input_matricula.pack(fill="x", padx=10)
+            input_matricula.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+        elif prop == "email":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_email = tk.Entry(modal_modify)
+            input_email.pack(fill="x", padx=10)
+            input_email.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+        elif prop == "telefono":
+            tk.Label(modal_modify, text=f"{prop.capitalize()}:").pack(anchor="w", padx=10, pady=(10, 0))
+            input_telefono = tk.Entry(modal_modify)
+            input_telefono.pack(fill="x", padx=10)
+            input_telefono.insert(0, current_data[HEADER_VETERINARIAN.index(prop)])
+    def save_data():
+        updated_entity = current_data.copy()
+        updated_entity[HEADER_VETERINARIAN.index("dni")] = input_dni.get()
+        updated_entity[HEADER_VETERINARIAN.index("nombre")] = input_name.get()
+        updated_entity[HEADER_VETERINARIAN.index("apellido")] = input_surname.get()
+        updated_entity[HEADER_VETERINARIAN.index("matricula")] = input_matricula.get()
+        updated_entity[HEADER_VETERINARIAN.index("email")] = input_email.get()
+        updated_entity[HEADER_VETERINARIAN.index("telefono")] = input_telefono.get()
+        update_data_veterinarian(updated_entity)
+        modal_modify.destroy()
+        load_dynamic_table(container, root)
+
+    btn_frame = tk.Frame(modal_modify, pady=20)
+    btn_frame.pack()
+    
+    tk.Button(btn_frame, text="Cancelar", command=modal_modify.destroy).pack(side="left", padx=5)
+    tk.Button(btn_frame, text="Guardar", bg="#4CAF50", fg="white", command=save_data).pack(side="left", padx=5)
+
+def delete_veterinarian(id, root, list_container):
     if messagebox.askyesno("Confirmar", f"¿Borrar ID {id}?"):
-        print(f"Eliminando ID: {id}")
+        delete_veterinarian_by_id(id)
+        load_dynamic_table(list_container, root)
 
-def load_dynamic_table(container):
+def load_dynamic_table(container, root, filtered_data=[]):
     for widget in container.winfo_children():
         widget.destroy()
 
@@ -30,9 +152,13 @@ def load_dynamic_table(container):
                  bg="gray", fg="white", anchor="w").pack(side="left", padx=5)
     
     tk.Label(header_frame, text="Acciones", bg="gray", fg="white").pack(side="right", padx=30)
-
-    data = show_all_veterinarians_action() 
     
+    data = []
+    if filtered_data:
+        data.append(filtered_data)
+    else:
+        data = show_all_veterinarians_action() 
+        
     for tuple_row in data:
         row_frame = tk.Frame(container, pady=2, bd=1, relief="solid")
         row_frame.pack(fill="x", pady=2)
@@ -47,11 +173,26 @@ def load_dynamic_table(container):
         btn_frame.pack(side="right", padx=5)
         
         tk.Button(btn_frame, text="Eliminar", bg="#ffcccc", 
-                  command=lambda i=row_id: delete_veterinarian(i)).pack(side="left", padx=2)
+                  command=lambda i=row_id: delete_veterinarian(i, root, container)).pack(side="left", padx=2)
         tk.Button(btn_frame, text="Editar", 
-                  command=lambda i=row_id: modify_veterinarian(i)).pack(side="left", padx=2)
+                  command=lambda i=row_id: modify_veterinarian(i, root, container)).pack(side="left", padx=2)
 
 def create_list_frame(root):
     list_frame = tk.Frame(root, padx=10, pady=10)
+    
+    frame_controles = tk.Frame(root, pady=10, padx=10, bg="#eee")
+    frame_controles.pack(fill="x")
+
+    tk.Label(frame_controles, text="Buscar:", bg="#eee").pack(side="left")
+
+    input_search = tk.Entry(frame_controles)
+    input_search.pack(side="left", padx=5)
+
+    btn_buscar = tk.Button(frame_controles, text="🔍", command=lambda: on_search_veterinarian(input_search, list_frame))
+    btn_buscar.pack(side="left")
+
+    btn_nuevo = tk.Button(frame_controles, text="+ Nuevo Usuario", bg="#4CAF50", fg="white", command=lambda: on_create_new_veterinarian(root, list_frame))
+    btn_nuevo.pack(side="right")
+    
     list_frame.pack(fill="both", expand=True)
-    load_dynamic_table(list_frame)
+    load_dynamic_table(list_frame, root=root)
